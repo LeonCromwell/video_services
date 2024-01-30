75ed1bbbbd7bbf7a154a609b7cc705cd6975eebc
LeonCrimson <mailyhai814@gmail.com>
2024-01-30 15:26:57 +0700

    đã convert được video lớn, lỗi đầu vào

--- src/app/middleware/convertHls.js
+++ src/app/middleware/convertHls.js
@@ -1,83 +1,67 @@
-const { spawn } = require("child_process");
+var FfmpegCommand = require("fluent-ffmpeg");
+
 const { storage, bucketName } = require("../../config/google");
 
-function convertBufferToHls(file, fileName) {
-  return new Promise((resolve, reject) => {
-    const ffmpeg = spawn(
-      "ffmpeg",
-      [
-        "-i",
-        "pipe:0",
-        "-vf",
-        "scale=640:-2",
-        "-c:v",
-        "h264",
-        "-c:a",
-        "aac",
-        "-hls_time",
-        "20",
-        "-hls_list_size",
-        "0",
-        "-f",
-        "hls",
-        "pipe:1",
-      ],
-      { stdio: ["pipe", "pipe", "pipe"] }
-    );
+async function convertBufferToHls(file, fileName) {
+  try {
+    const resolutions = [
+      {
+        resolution: "320x180",
+        videoBitrate: "500k",
+        audioBitrate: "64k",
+      },
+      {
+        resolution: "854x480",
+        videoBitrate: "1000k",
+        audioBitrate: "128k",
+      },
+      {
+        resolution: "1280x720",
+        videoBitrate: "2500k",
+        audioBitrate: "192k",
+      },
+    ];
 
-    const m3u8Destination = `videos/hls_video/${fileName}/output.m3u8`;
-    const tsDestination = `videos/hls_video/${fileName}/output%03d.ts`;
-    // Create a Write Stream for .m3u8 file
-    const m3u8UploadStream = storage
-      .bucket(bucketName)
-      .file(m3u8Destination)
-      .createWriteStream({
-        resumable: true,
-        metadata: {
-          contentType: "application/vnd.apple.mpegurl", // Set the correct content type for M3U8
-        },
+    for (const { resolution, videoBitrate, audioBitrate } of resolutions) {
+      console.log(`Hls ${resolution} started`);
+      let mp4FileName = file.originalname.split(".")[0];
+      const outputFileName = `${mp4FileName.replace(
+        ".",
+        "_"
+      )}_${resolution}.m3u8`;
+      const segmentFileName = `${mp4FileName.replace(
+        ".",
+        "_"
+      )}_${resolution}_%03d.ts`;
+      console.log(file.buffer);
+      await new Promise((resolve, reject) => {
+        FfmpegCommand()
+          .outputOptions([
+            `-c:v h264`,
+            `-b:v ${videoBitrate}`,
+            `-c:a aac`,
+            `-b:a ${audioBitrate}`,
+            `-vf scale=${resolution}`,
+            `-f hls`,
+            `-hls_time 10`,
+            `-hls_list_size 0`,
+            `-hls_segment_filename ./hls/${segmentFileName}`,
+          ])
+          .output(`./hls/${outputFileName}`)
+          .on("end", () => {
+            console.log(`Hls ${resolution} finished`);
+            resolve();
+          })
+          .on("error", (error) => {
+            console.log(`Hls ${resolution} error`);
+            reject(error);
+          })
+          .run();
       });
-
-    // Create a new pipe for .ts files
-    const tsUploadStream = ffmpeg.stdout.pipe(
-      storage
-        .bucket(bucketName)
-        .file(tsDestination)
-        .createWriteStream({
-          resumable: true,
-          metadata: {
-            contentType: "video/MP2T", // Set the correct content type for TS segments
-          },
-        })
-    );
-
-    // Pipe FFmpeg output directly to Google Cloud Storage for .m3u8
-    ffmpeg.stdout.pipe(m3u8UploadStream);
-
-    m3u8UploadStream.on("error", (error) => {
-      console.error("Error uploading .m3u8 to Google Cloud Storage:", error);
-      reject(error);
-    });
-
-    m3u8UploadStream.on("finish", () => {
-      console.log(".m3u8 Upload to Google Cloud Storage complete.");
-    });
-
-    // Handle errors and logs
-    ffmpeg.stderr.on("data", (data) => {
-      console.log("ffmpeg stderr data", data.toString());
-    });
-    ffmpeg.stdout.on("data", (data) => {
-      console.log("ffmpeg stdout data", data.toString());
-    });
-    ffmpeg.stdout.on("end", () => {
-      console.log("ffmpeg stdout ended");
-      // Xử lý việc kết thúc dữ liệu đầu ra của FFmpeg ở đây
-    });
-
-    ffmpeg.stdin.write(file.buffer);
-    ffmpeg.stdin.end();
-  });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = convertBufferToHls;
